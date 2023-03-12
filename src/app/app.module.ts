1ddbb66eb94573e115fc9397dbb18ea010e0bf5f
Felipe Sales <felipesales007@hotmail.com>
2023-03-12 19:56:50 -0300

    prettierrc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,20 +14,8 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent,
-    SobreComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    RippleModule,
-    ButtonModule,
-  ],
+  declarations: [AppComponent, InicioComponent, SobreComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, RippleModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
