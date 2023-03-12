ca03661867fc4bba26980a2b5b7a21b159b0e9bc
Felipe Sales <felipesales007@hotmail.com>
2023-03-12 18:31:55 -0300

    limpando o código

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'primeng'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('primeng');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'primeng app is running!'
-    );
-  });
 });
