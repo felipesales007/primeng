fe489cd87d579872523b8501811bfd602c6deab4
Felipe Sales <felipesales007@hotmail.com>
2023-03-12 18:46:11 -0300

    criando telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InicioComponent, SobreComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
