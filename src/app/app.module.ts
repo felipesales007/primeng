b706ba97b74f7a67eac88bca5c2c176e7b144738
Felipe Sales <felipesales007@hotmail.com>
2023-03-12 19:53:27 -0300

    importando animações, ripple e botões

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
+
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { HeaderComponent } from './components/layout/header/header.component';
@@ -17,7 +21,13 @@ import { FooterComponent } from './components/layout/footer/footer.component';
     HeaderComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RippleModule,
+    ButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
